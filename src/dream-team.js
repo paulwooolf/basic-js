3882e6f4045252ea39fcafd520ea2f9719afcd0c
pvolkov <pvolkov@ndv.ru>
2021-03-15 13:29:02 +0300

    feat: dream-team task is completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members = []) {
+    if (Array.isArray(members) === false) return false;
+    const result = members.filter(name => typeof name === 'string');
+    return result.map(name => name.trim().charAt(0).toUpperCase()).sort().join('');
 };
