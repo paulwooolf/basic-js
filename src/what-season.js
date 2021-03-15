59738ef47aadba11833138b06d938b406ba85866
paulwooolf <paulwooolf@gmail.com>
2021-03-15 23:22:30 +0300

    feature: write what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {
+    const monthAssoc = {
+        '0': 'winter',
+        '1': 'winter',
+        '2': 'spring',
+        '3': 'spring',
+        '4': 'spring',
+        '5': 'summer',
+        '6': 'summer',
+        '7': 'summer',
+        '8': 'autumn',
+        '9': 'autumn',
+        '10': 'autumn',
+        '11': 'winter'
+    };
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    if (!date) return 'Unable to determine the time of year!';
+    date.toLocaleString();
+    if (date instanceof Date) return monthAssoc[date.getMonth()];
+    throw new Error();
 };
