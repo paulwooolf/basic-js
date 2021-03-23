536f5f8f20aacc56f64154a03bd6d3c38860767f
paulwooolf <paulwooolf@gmail.com>
2021-03-24 00:49:48 +0300

    feat: extended start

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// Ваша задача — реализовать функцию repeater(str, options). Эта функция возвращает повторяющуюся строку, основываясь на заданных параметрах:
+//
+//     str это строка, которая будет повторена
+// options это объект опций, который содержит следующие свойства:
+//     repeatTimes устанавливает число повторений str
+// separator это строка, разделяющая повторения str
+// addition это дополнительная строка, которая будет добавлена после каждого повторения str
+// additionRepeatTimes устанавливает число повторений addition
+// additionSeparator это строка, разделяющая повторения addition
+// Параметры str и addition по умолчанию являются строками. В случае, если они другого типа, он должны быть преобразованы к строке.
+//
+//     Параметры separator и additionSeparator являются строками.
+//
+//     repeatTimes и additionRepeatTimes являются целыми числами (в случае отсутствия любого из них соответствующая строка не повторяется).
+//
+// Единственный обязательный параметр — это str, остальные могут не быть переданы. Значение separator по умолчанию это '+'. Значение additionSeparator по умолчанию это '|'.
+
+module.exports = function repeater(str, options = {
+    repeatTimes: 0,
+    separator: '+',
+    addition: '',
+    additionRepeatTimes: 0,
+    additionSeparator: '|'
+}) {
+
 };
-  
