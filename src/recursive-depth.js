a53700eed6f686d086eb0e4c96a6da8e38e0f5df
paulwooolf <paulwooolf@gmail.com>
2021-03-25 01:02:11 +0300

    feat: recursive depth is completed

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,58 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
+    static n = 0;
     calculateDepth(arr) {
-        if (Array.isArray(arr) === false) {
-            return 0;
-        }
-        function rec(arr, n) {
-            const next = [];
-            let isDepth = false;
-            arr.forEach((item) => {
-                if (Array.isArray(item)) {
-                    next.push(...item);
-                    isDepth = true;
-                }
-            })
-            if (isDepth) {
-                n = rec(next, ++n);
+        if (Array.isArray(arr) === false) return DepthCalculator.n;
+        else DepthCalculator.n++;
+        const next = [];
+        let isDepth = false;
+        arr.forEach((item) => {
+            if (Array.isArray(item)) {
+                next.push(...item);
+                isDepth = true;
             }
-            return n;
-        }
-        return rec(arr, 1);
+        })
+        if (isDepth) DepthCalculator.n = this.calculateDepth(next);
+        let result = DepthCalculator.n;
+        DepthCalculator.n = 0;
+        return result;
     }
 };
-
-class DepthCalculator {
-    calculateDepth(arr) {
-        if (Array.isArray(arr) === false) {
-            return 0;
-        }
-        function rec(arr, n) {
-            const next = [];
-            let isDepth = false;
-            arr.forEach((item) => {
-                if (Array.isArray(item)) {
-                    next.push(...item);
-                    isDepth = true;
-                }
-            })
-            if (isDepth) {
-                n = rec(next, ++n);
-            }
-            return n;
-        }
-        return rec(arr, 1);
-    }
-};
-
-const depth = new DepthCalculator();
-console.log(depth.calculateDepth([1, 2, 3, 4, 5, [1, []]]), 3);
-console.log(depth.calculateDepth([[[[[]]]]]), 5);
-console.log(depth.calculateDepth(1), 0);
-// console.log(depth.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);
-// console.log(depth.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 5);
-// console.log(depth.calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 15);
-// console.log(depth.calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 25);
-// console.log(depth.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 31);
-
