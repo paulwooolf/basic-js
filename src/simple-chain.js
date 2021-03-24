c960542257d5f555b98fee973088e0f7806a177f
paulwooolf <paulwooolf@gmail.com>
2021-03-25 01:35:04 +0300

    feat: simple chain is process

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    line: [],
+    getLength() {
+        return this.line.length;
+    },
+    addLink(value) {
+        value = '( ' + value + ' )';
+        this.line.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (this.line[position] === undefined) {
+            this.line = [];
+            throw new Error();
+        }
+        this.line.splice(position, 1);
+        return this;
+    },
+    reverseChain() {
+        this.line = this.line.reverse();
+        return this;
+    },
+    finishChain() {
+        const result = this.line.join('~~');
+        this.line = [];
+        return result;
+    }
 };
 
 module.exports = chainMaker;
