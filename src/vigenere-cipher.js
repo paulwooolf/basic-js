8cd4106a1726f173b7f177fb2b578bee55a5ceb2
paulwooolf <paulwooolf@gmail.com>
2021-03-26 01:03:13 +0300

    feat: vigenere cipher in process

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(type = true) {
+        this.type = type;
+    }
+
+    modifyKey(message, key) {
+        if (key.length >= message.length) {
+            return key;
+        }
+        const k = Math.ceil(message.length / key.length);
+        let newKey = '';
+        for (let i = 0; i < k; i++) {
+            newKey += key;
+        }
+        const newArr = newKey.split('');
+        const result = [];
+        message.split('').forEach(item => {
+            if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 92) {
+                result.push(newArr.shift());
+            } else {
+                result.push(' ');
+            }
+        })
+        return result;
+    }
+
+    encrypt(message, key) {
+        if (message === undefined || key === undefined) {
+            throw new Error();
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let newKey = this.modifyKey(message, key);
+        let crypt = [];
+        message.split('').forEach((item, index) => {
+            if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 91) {
+                crypt.push(String.fromCharCode(((item.charCodeAt(0) + newKey[index].charCodeAt(0)) % 26) + 65))
+            } else {
+                crypt.push(item);
+            }
+        });
+        return this.type === true ? crypt.join('') : crypt.reverse().join();
+    }
+
+    decrypt(message, key) {
+        if (message === undefined || key === undefined) {
+            throw new Error();
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let newKey = this.modifyKey(message, key);
+        let crypt = [];
+        message.split('').forEach((item, index) => {
+            if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 91) {
+                crypt.push(String.fromCharCode(((item.charCodeAt(0) + 26 - newKey[index].charCodeAt(0)) % 26) + 65))
+            } else {
+                crypt.push(item);
+            }
+        });
+        return this.type === true ? crypt.join('') : crypt.reverse().join();
+    }
 }
+
+// const machine = new VigenereCipheringMachine(true);
+// console.log(machine.encrypt('5,,,!', 'js'));
+// console.log(machine.decrypt('5,,,!', 'js'));
+// console.log(machine.decrypt('ICWWQAM KECEIK JVZZT EADGG!', 'rollingscopes'), 'ROLLING SCOPES SHOOL RULES!');
+// console.log(machine.decrypt('TRVVFB VT JSUIFMYL!', 'learning'), 'INVEST IN YOURSELF!');
+// console.log(machine.decrypt('HSVD AJAL ^^', 'behappy'), 'GOOD LUCK ^^');
 
 module.exports = VigenereCipheringMachine;
