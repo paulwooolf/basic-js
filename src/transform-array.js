308473bae2376529805b1ffd9184e26b53dae57d
paulwooolf <paulwooolf@gmail.com>
2021-03-28 01:29:29 +0300

    feat: transform array is completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error();
+
+    function isAccess(item) {
+        return item !== undefined && item !== '--discard-next' && item !== '--discard-prev' && item !== '--double-next' && item !== '--double-prev';
+    }
+
+    const newArr = [...arr];
+    arr.forEach((item, index) => {
+        if (item === '--discard-next') {
+            newArr[index] = undefined;
+            if (isAccess(newArr[index+1])) {
+                newArr[index+1] = undefined;
+            }
+        } else if (item === '--discard-prev') {
+            newArr[index] = undefined;
+            if (isAccess(newArr[index-1])) {
+                newArr[index-1] = undefined;
+            }
+        } else if (item === '--double-next') {
+            if (isAccess(newArr[index+1])) {
+                newArr[index] = newArr[index+1];
+            } else {
+                newArr[index] = undefined;
+            }
+        } else if (item === '--double-prev') {
+            if (isAccess(newArr[index-1])) {
+                newArr[index] = newArr[index-1];
+            } else {
+                newArr[index] = undefined;
+            }
+        }
+    });
+    return newArr.filter(item => item !== undefined);
 };
