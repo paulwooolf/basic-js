8d18c99ada6f7a3f603361da5d957f869fdddd23
paulwooolf <paulwooolf@gmail.com>
2021-03-29 21:29:44 +0300

    fix: delete key modify method

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,27 +5,6 @@ class VigenereCipheringMachine {
         this.type = type;
     }
 
-    modifyKey(message, key) {
-        if (key.length >= message.length) {
-            return key;
-        }
-        const k = Math.ceil(message.length / key.length);
-        let newKey = '';
-        for (let i = 0; i < k; i++) {
-            newKey += key;
-        }
-        const newArr = newKey.split('');
-        const result = [];
-        message.split('').forEach(item => {
-            if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 92) {
-                result.push(newArr.shift());
-            } else {
-                result.push(' ');
-            }
-        })
-        return result;
-    }
-
     encrypt(message, key) {
         if (message === undefined || key === undefined) {
             throw new Error();
@@ -33,11 +12,16 @@ class VigenereCipheringMachine {
         message = message.toUpperCase();
         key = key.toUpperCase();
 
-        let newKey = this.modifyKey(message, key);
+        while (key.length < message.length) {
+            key += key;
+        }
+        let newKey = key.split('');
+
         let crypt = [];
-        message.split('').forEach((item, index) => {
+        message.split('').forEach((item) => {
             if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 91) {
-                crypt.push(String.fromCharCode(((item.charCodeAt(0) + newKey[index].charCodeAt(0)) % 26) + 65))
+                let k = newKey.shift();
+                crypt.push(String.fromCharCode(((item.charCodeAt(0) + k.charCodeAt(0)) % 26) + 65))
             } else {
                 crypt.push(item);
             }
@@ -52,11 +36,15 @@ class VigenereCipheringMachine {
         message = message.toUpperCase();
         key = key.toUpperCase();
 
-        let newKey = this.modifyKey(message, key);
+        while (key.length < message.length) {
+            key += key;
+        }
+        let newKey = key.split('');
         let crypt = [];
-        message.split('').forEach((item, index) => {
+        message.split('').forEach((item) => {
             if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 91) {
-                crypt.push(String.fromCharCode(((item.charCodeAt(0) + 26 - newKey[index].charCodeAt(0)) % 26) + 65))
+                let k = newKey.shift();
+                crypt.push(String.fromCharCode(((item.charCodeAt(0) + 26 - k.charCodeAt(0)) % 26) + 65))
             } else {
                 crypt.push(item);
             }
